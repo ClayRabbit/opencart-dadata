9fb2b0fff5ac67bea9645bc9d04d3d86150b3aa4
Andrey Chesnakov <garlic@mail.ru>
2022-11-06 21:09:59 +0500

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    outputDir: './admin/view/javascript/dadata-opencart/',
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/admin/view/javascript/dadata-opencart/'
-        : '/'
-}
